78435ed89d4d15e0860820aed952ed506604d934
stedzi-codes <steff.codes@proton.me>
2026-06-06 16:45:08 +0200

    add oswald font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import "./globals.scss";
+
+const oswald = localFont({
+  src: [
+    { path: "./fonts/oswald-400.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/oswald-500.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/oswald-600.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/oswald-700.ttf", weight: "700", style: "normal" },
+  ],
+  variable: "--font-oswald",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Oslo Roller Derby",
@@ -14,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={oswald.variable}>
       <body suppressHydrationWarning>
         <Header />
         <main className="site-content">{children}</main>
