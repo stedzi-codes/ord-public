e14ced4634bedf2d4f58675ae47d735f38cffcc1
stedzi-codes <steff.codes@proton.me>
2026-06-07 22:06:32 +0200

    add prettier

---
+++ src/app/content/teams.ts
@@ -0,0 +1,26 @@
+export const aTeamData = {
+    roster: [
+  { number: "108", name: "Kiki Longskates" },
+  { number: "1312", name: "Pikante" },
+  { number: "16", name: "Hell" },
+  { number: "2", name: "Feline" },
+  { number: "242", name: "Winter Wolf" },
+  { number: "37", name: "Bosies" },
+  { number: "412", name: "Raine Check" },
+  { number: "42", name: "Panserbinna" },
+  { number: "455", name: "Rosie the Ruiner" },
+  { number: "46", name: "Darkmess" },
+  { number: "51", name: "Ally McWheel" },
+  { number: "59", name: "Katla Nightmar" },
+  { number: "6", name: "Just Jeff" },
+  { number: "64", name: "Jenny Vidi Vici" },
+  { number: "73", name: "Hey Doodle" },
+  { number: "79", name: "Torture" },
+  { number: "82", name: "Bloody L" },
+  { number: "911", name: "Moxie Fire" },
+  { number: "933", name: "Killer Rosenkål" },
+]
+}
+
+
+
